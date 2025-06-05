b9e7a493bc166d0be9b7e4b39f8dfc1e9fba75e8
Shaurya Infotech <developer@shaurya-infotech.com>
2025-06-05 12:43:56 +0530

    Add files via upload
    first commit

---
+++ custom/addons/mfr/mfr/customs/addons/mrp_estimation/models/__init__.py
@@ -0,0 +1,6 @@
+from . import estimation
+from . import estimation_line
+from . import estimation_cost
+from . import estimation_version
+from . import mrp_costing
+from . import res_config_settings
