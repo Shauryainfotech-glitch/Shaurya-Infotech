48b2716172955de9eedb676d6e8150d3f06a887a
rushikesh <rushijawale9960@gmail.com>
2025-06-06 14:57:19 +0530

    changes

--- customs/addons/day_plan_work_report_ai/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import day_plan
-from . import day_plan_task
-from . import work_report
-from . import ai_analysis
-from . import dashboard
--- customs/addons/day_plan_work_report_ai/report/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import dashboard_report
-from . import dashboard_pdf_report
