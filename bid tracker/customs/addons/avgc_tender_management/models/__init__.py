6e9ef42ff3878a3dbbaf04a57eaf2a3634c55e14
Shaurya Infotech <developer@shaurya-infotech.com>
2025-06-10 15:30:47 +0530

    Delete bid tracker/customs/addons/avgc_tender_management directory

--- bid tracker/customs/addons/avgc_tender_management/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import tender
-from . import vendor
-from . import gem_bid
-from . import document_management
-from . import ai_analysis
-from . import finance_transaction
-from . import task_management
--- bid tracker/customs/addons/avgc_tender_management/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import ai_analysis_wizard
-from . import tender_processing_wizard
-from . import document_reject_wizard
-from . import vendor_blacklist_wizard
-from . import task_create_wizard
