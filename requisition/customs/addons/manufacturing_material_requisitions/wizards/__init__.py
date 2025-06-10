5d950e0d7a8763cac41c9ef3c8683fd8cfe824be
Shaurya Infotech <developer@shaurya-infotech.com>
2025-06-10 15:32:49 +0530

    Delete requisition directory

--- requisition/customs/addons/manufacturing_material_requisitions/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import bulk_requisition_wizard
-# from . import emergency_requisition_wizard  # Placeholder - to be implemented
-# from . import mrp_requisition_wizard  # Placeholder - to be implemented
-# from . import vendor_selection_wizard  # Placeholder - to be implemented
-# from . import material_shortage_wizard  # Placeholder - to be implemented 
