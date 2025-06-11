7c267f4c505dfab81c57a3805c18428d44eb3d46
rushikesh <rushijawale9960@gmail.com>
2025-06-11 10:19:59 +0530

    updates

--- customs/addons/mrp_estimation/models/res_config_settings.py
+++ customs/addons/mrp_estimation/models/res_config_settings.py
@@ -6,36 +6,30 @@ class ResConfigSettings(models.TransientModel):
 
     estimation_sequence_prefix = fields.Char(
         string='Estimation Sequence Prefix',
-        config_parameter='mrp_estimation.sequence_prefix',
-        default='EST'
+        config_parameter='mrp_estimation.sequence_prefix'
     )
 
     auto_markup_enabled = fields.Boolean(
         string='Enable Auto Markup',
-        config_parameter='mrp_estimation.auto_markup',
-        default=False
+        config_parameter='mrp_estimation.auto_markup'
     )
 
     approval_required = fields.Boolean(
         string='Require Approval Workflow',
-        config_parameter='mrp_estimation.approval_required',
-        default=True
+        config_parameter='mrp_estimation.approval_required'
     )
 
     version_increment = fields.Float(
         string='Version Increment Value',
-        config_parameter='mrp_estimation.version_increment',
-        default=0.1
+        config_parameter='mrp_estimation.version_increment'
     )
 
     default_material_markup = fields.Float(
         string='Default Material Markup (%)',
-        config_parameter='mrp_estimation.default_material_markup',
-        default=10.0
+        config_parameter='mrp_estimation.default_material_markup'
     )
 
     default_cost_markup = fields.Float(
         string='Default Cost Markup (%)',
-        config_parameter='mrp_estimation.default_cost_markup',
-        default=15.0
+        config_parameter='mrp_estimation.default_cost_markup'
     )
